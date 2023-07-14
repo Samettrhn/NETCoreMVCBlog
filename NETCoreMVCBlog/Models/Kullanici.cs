75f48066f64196380cbc0a7db3aeaa4b3700ef52
Samet Turhan <sametturhan2646@gmail.com>
2023-07-14 18:06:01 +0300

    Yeni property eklendi

--- NETCoreMVCBlog/Models/Kullanici.cs
+++ NETCoreMVCBlog/Models/Kullanici.cs
@@ -5,6 +5,7 @@
 
             public int Id { get; set; }
             public string Ad { get; set; }
+            public string Soyad { get; set; }
             public string KullaniciAdi { get; set; }
             public string Email { get; set; }
 
